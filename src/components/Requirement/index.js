785521027aed8c37cebc95e47f73ab912183e446
Samuel Costa <samuel.costa@ccc.ufcg.edu.br>
2022-03-28 19:15:16 -0300

    criação do modal e esboço das funções

--- src/components/Requirement/index.js
+++ src/components/Requirement/index.js
@@ -1,5 +1,10 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import moment from 'moment';
+
+import { api } from '../../service/api';
+
+import ThumbUpAltOutlinedIcon from "@mui/icons-material/ThumbUpAltOutlined";
+import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 
 import './styles.css';
 
@@ -8,6 +13,8 @@ import Modal from '../Modal';
 export default function Requirement({requirement}) {
     const showReqModalRef = useRef();
     const requirementUser = requirement.user;
+    const [liked, setLiked] = useState(false);
+    const [comment, setComment] = useState('');
 
     function renderRequirementStatus() {
         if (requirement.status === "concluded") {
@@ -28,6 +35,21 @@ export default function Requirement({requirement}) {
                     <span>Em avaliação</span>
                 </div>
             )
+        }
+    }
+
+    function clickLike() {
+        setLiked(!liked);
+        if (liked) {
+            api.delete('/curtida', requirementUser);
+        } else {
+            api.post('/curtida', requirementUser);
+        }
+    }
+
+    function clickComment() {
+        if (comment !== '') {
+            api.post('/comentario', requirementUser, comment);
         }
     }
 
@@ -59,7 +81,91 @@ export default function Requirement({requirement}) {
 
             </div>
             <Modal ref={showReqModalRef}>
-                <h1>Visualizar requerimento</h1>    
+                <section className="requirement_modal_title">
+                    <h1>Visualização de requerimento</h1>
+                </section>
+                <section className="requirement_modal_user">
+                    <div className="requirement_modal_profile">
+                        <img src="/profile.png" alt="user-img" />
+                        <div className="requirement_modal_user_info">
+                            <span><strong>{requirementUser.name}</strong></span>
+                            <span>{requirementUser.location}</span>
+                        </div>
+                    </div>
+                    <div className="requirement_modal_ocourr_date">
+                        <span><strong>Data do ocorrido:</strong> {moment.unix(requirement.creationDate).format("DD/MM/YYYY")}</span>
+                    </div>
+                </section>
+                <section className="requirement_modal_status">
+                    <div>
+                        <span className="requirement_modal_status_user">Requerimento do Usuário {requirementUser.name}</span>
+                        <div className="requirement_modal_status_date"><strong>Criado em: </strong> {moment.unix(requirement.creationDate).format("DD/MM/YYYY")}</div>
+                    </div>
+                    {renderRequirementStatus()}
+                </section>
+                <div className="requirement_modal_tags">
+                    {requirement.tags?.map((tag, index) => (
+                        <div key={index} className="requirement_modal_tag">
+                            {tag}
+                            <div>+</div>
+                        </div>
+                    ))}
+                </div>
+                <div className="requirement_modal_message">
+                    {requirement.message}
+                </div>
+                <div className="requirement_modal_likes">
+                    { liked ?
+                        <ThumbUpIcon style={{ color: 'var(--blue)', width: '20px', cursor: 'pointer'}} onClick={clickLike} />
+                    :
+                        <ThumbUpAltOutlinedIcon style={{ color: 'var(--descriptions)', width: '20px', cursor: 'pointer'}} onClick={clickLike} />
+                    }
+                    
+                    <div>{requirement.likes} Apoios</div>
+                </div>
+                <div className="requirement_modal_midia_comments">
+                    <section className="requirement_modal_midias">
+                        <span><strong>Lista de mídias</strong></span>
+                        <div>
+                            {requirement.media?.map((mda, index) => (
+                                <img src={mda} key={index} alt={`media${index}`} className="image_cropped" />
+                            ))}
+                        </div>
+                    </section>
+                    <section className="requirement_modal_comments">
+                        <span><strong>Comentários</strong></span>
+                        <div className="requirement_modal_comment">
+                            {requirement.comments?.map((comment, index) => (
+                                <>
+                                    <div key={index} className="requirement_modal_comment_user">
+                                        <img src={comment.profile} alt={`Foto de ${comment.name}`} className="image_profiles" />
+                                        <span><strong>{comment.name}</strong></span>
+                                    </div>
+                                    <div className="requirement_modal_comment_message">
+                                        {comment.message}
+                                    </div>
+                                </>
+                            ))}
+                        </div>
+                    </section>
+                </div>
+                <div className="requirement_modal_legislators_comment">
+                    <section>
+                        <span><strong>Legisladores associados</strong></span>
+                        <div className="requirement_modal_legislators">
+                            {requirement.legislators?.map((legislator, index) => (
+                                <div key={index} className="requirement_modal_legislator">
+                                    <span><strong>{legislator.name}</strong></span>
+                                    <span style={{ color: 'var(--descriptions)' }}>{legislator.party}</span>
+                                </div>
+                            ))}
+                        </div>
+                    </section>
+                    <section className="requirement_modal_comment_input">
+                        <input type='text' value={comment} onChange={(e) => setComment(e.target.value)}/>
+                        <button type='submit' onClick={clickComment}>Comentar</button>
+                    </section>
+                </div>
             </Modal>
         </>
     )
